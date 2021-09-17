1050cc2a77342507f5b4ff6a76feb1a4e1e61bec
frengki83 <55172042+frengki83@users.noreply.github.com>
2021-09-17 12:22:09 +0700

    Update __main__.py

--- SongBot/__main__.py
+++ SongBot/__main__.py
@@ -8,15 +8,11 @@ from SongBot import app, LOGGER
 from SongBot.SongBot import ignore_blacklisted_users
 from SongBot.sql.chat_sql import add_chat_to_db
 
-cari_text = """
+start_text = """
 Hello [{}](tg://user?id={}),
+\n\nI'm Indo Music Bot
 
-\n\nI'm SongBot
-
-I'M Music Bot
-
-Send The Name of The Song You Want
-
+Send Me The Name of the Song You Want
 𝐄x. ```/song Music```
 """
 
@@ -28,14 +24,13 @@ owner_help = """
 /chatlist get list of all chats
 """
 
-
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("Yes"))
-async def Yes(client, message):
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("start"))
+async def start(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     name = message.from_user["first_name"]
-    await message.reply(cari_text.format(name, user_id), add_chat_to_db(str(chat_id))
-
+    await message.reply(start_text.format(name, user_id))
+    add_chat_to_db(str(chat_id))
     
 @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
 async def help(client, message):
@@ -45,6 +40,15 @@ async def help(client, message):
     text = "Cari Musik yang anda inginkan \n Type /song (song name)"
     await message.reply(text)
 
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("song"))
+async def song(client, message):
+    if message.from_user["id"] == OWNER_ID:
+        await message.reply(owner_help)
+        return ""
+    text = "Please Type your song"
+    await message.reply(text)
+
+    
 OWNER_ID.append(1492186775)
 app.start()
 LOGGER.info("SongBot Is Running")
